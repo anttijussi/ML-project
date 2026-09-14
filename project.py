428e9f9065663de0844087a2d496c1e1185f9403
Anttijussi Karjalainen <anttijussi.karjalainen@proton.me>
2026-09-14 20:29:34 +0300

    data lisätty + luettu tiedostoon

--- project.py
+++ project.py
@@ -1,4 +1,9 @@
 import numpy as np
 import pandas as pd
 import sklearn as scikit_learn
-   
+
+
+datared = pd.read_csv('winequality-red.csv')
+datawhite = pd.read_csv('winequality-white.csv')
+
+datared.head(5)
