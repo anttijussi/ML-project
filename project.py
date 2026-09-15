edf39ec820b091e41c76b4a17ab072f1a1dc75b2
Anttijussi Karjalainen <anttijussi.karjalainen@proton.me>
2026-09-15 18:19:34 +0300

    valkoviini lisätty

--- project.py
+++ project.py
@@ -14,8 +14,17 @@ y_white = datawhite["quality"]
 X_red = datared.drop(columns = "quality")
 X_white = datawhite.drop(columns = "quality")
 
-reg = LinearRegression()
-reg.fit(X_red, y_red)
-y_red_pred = reg.predict(X_red)
-tr_error = mean_squared_error(y_red, y_red_pred)
-print(tr_error)
+
+# Red wine
+reg1 = LinearRegression()
+reg1.fit(X_red, y_red)
+y_red_pred = reg1.predict(X_red)
+tr_error_red = mean_squared_error(y_red, y_red_pred)
+print("Accuracy of red: ", tr_error_red)
+
+# White wine
+reg2 = LinearRegression()
+reg2.fit(X_white, y_white)
+y_white_pred = reg2.predict(X_white)
+tr_error_white = mean_squared_error(y_white, y_white_pred)
+print("Accuracy of white: ", tr_error_white)
